57110d46afb112197c08c111a4b224da99708452
Gerardo Pozo <jgpozo@espol.edu.ec>
2022-01-13 12:35:47 -0500

    Update 4
    - Creación de ventana principal con menú de opciones
    - Controlador actualizado, sin cambios de escena por el momento

---
+++ concursoMascotaV2/src/main/java/ec/edu/espol/controllers/VentanaPrincipalFXMLController.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espol.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * FXML Controller class
+ *
+ * @author gerar
+ */
+public class VentanaPrincipalFXMLController implements Initializable {
+
+    @FXML
+    private Button btnDuenio;
+    @FXML
+    private Button btnMascota;
+    @FXML
+    private Button btnConcurso;
+    @FXML
+    private Button btnPremio;
+    @FXML
+    private Button btnCriterio;
+    @FXML
+    private Button btnInscripcion;
+    @FXML
+    private Button btnMiembroJurado;
+    @FXML
+    private Button btnEvaluacion;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void escenaDuenio(MouseEvent event) {
+    }
+
+    @FXML
+    private void escenaMascota(MouseEvent event) {
+    }
+
+    @FXML
+    private void escenaConcurso(MouseEvent event) {
+    }
+
+    @FXML
+    private void escenaPremio(MouseEvent event) {
+    }
+
+    @FXML
+    private void escenaCriterio(MouseEvent event) {
+    }
+
+    @FXML
+    private void escenaInscripcion(MouseEvent event) {
+    }
+
+    @FXML
+    private void escenaMiembroJurado(MouseEvent event) {
+    }
+
+    @FXML
+    private void escenaEvaluacion(MouseEvent event) {
+    }
+    
+}
