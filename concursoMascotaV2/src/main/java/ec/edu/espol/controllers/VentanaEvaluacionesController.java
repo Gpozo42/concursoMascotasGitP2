70f64f2b79a6e53b9a544672861d5ff2ca0d3c2a
Gerardo Pozo <jgpozo@espol.edu.ec>
2022-01-13 11:57:32 -0500

    Update 3
    Cambios para controller de ventanaEvaluacion:
    - Añadida función de limpieza de texto
    - Añadido creación de objecto Evaluación por medio de los datos en campos de texto (Sin probar)
    - Añadido función para cancelar el ingreso y retornar a la pantalla principal (Sin terminar)

--- concursoMascotaV2/src/main/java/ec/edu/espol/controllers/VentanaEvaluacionesController.java
+++ concursoMascotaV2/src/main/java/ec/edu/espol/controllers/VentanaEvaluacionesController.java
@@ -5,9 +5,18 @@
  */
 package ec.edu.espol.controllers;
 
+import ec.edu.espol.model.Evaluacion;
+import ec.edu.espol.model.Util;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 /**
  * FXML Controller class
@@ -15,6 +24,21 @@ import javafx.fxml.Initializable;
  * @author gerar
  */
 public class VentanaEvaluacionesController implements Initializable {
+    
+    private ArrayList<Evaluacion> evaluaciones;
+
+    @FXML
+    private Button btnHecho;
+    @FXML
+    private Button btnCancelar;
+    @FXML
+    private TextField txtEmailJurado;
+    @FXML
+    private TextField txtIdInscripcion;
+    @FXML
+    private TextField txtIdCriterio;
+    @FXML
+    private TextField txtNotaEvaluacion;
 
     /**
      * Initializes the controller class.
@@ -22,6 +46,22 @@ public class VentanaEvaluacionesController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        this.evaluaciones = new ArrayList<>();
     }    
+
+    @FXML
+    private void hecho(MouseEvent event) {
+        try {
+            int idInscripcion = Integer.parseInt(txtIdInscripcion.getText());
+            Evaluacion e = new Evaluacion(Util.nextID("evaluaciones.txt"), idInscripcion);
+        } 
+        catch (NullPointerException e) {
+            Alert a = new Alert(AlertType.WARNING, "Falta de ingresar datos");
+        }
+    }
+
+    @FXML
+    private void cancelar(MouseEvent event) {
+    }
     
 }
